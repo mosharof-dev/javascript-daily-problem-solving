e6f2f9f1a8ebd7471509e6311e4d0c24138cfa98
programmermosharof <mosharof.dev@gmail.com>
2026-02-27 00:00:36 +0600

    problem 2 sloved

--- day-2problem.js
+++ day-2problem.js
@@ -5,3 +5,10 @@ let password = "mosharof123"
     }else{
         console.log('Weak password');
     }
+const numbers = [10, 25, 8, 47, 3, 99, 15];
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 20){
+        console.log(numbers[i]);
+    }
+}
