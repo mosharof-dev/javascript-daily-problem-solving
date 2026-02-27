b4613d65a38747d52c6c4405c8492ae4842b7c2e
programmermosharof <mosharof.dev@gmail.com>
2026-02-27 23:22:53 +0600

    Day 3 prblem 2 sloved

--- day-03-problem.js
+++ day-03-problem.js
@@ -1,5 +1,5 @@
-// Ei array er sob numbers add kore 
-// total price print koro
+// // Ei array er sob numbers add kore 
+// // total price print koro
 
 
 const prices = [120, 450, 80, 320, 95];
@@ -12,3 +12,12 @@ for(let i = 0; i < prices.length; i++){
 console.log("Total Price : ", totalPrice);
 
 
+const scores = [45, 92, 67, 88, 23, 99, 54];
+
+let highestScore = scores[0];
+
+for(let i = 1; i < scores.length; i++){ 
+    if(scores[i] > highestScore){
+        highestScore = scores[i];
+    }}
+console.log("Highest Score : ", highestScore);
