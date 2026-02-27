09fe5998bca296813b5c188b6091cb41b53863ee
programmermosharof <mosharof.dev@gmail.com>
2026-02-28 00:23:34 +0600

    Day 3 Prblem Sloveing Complete

--- day-03-problem.js
+++ day-03-problem.js
@@ -1,5 +1,3 @@
-// // Ei array er sob numbers add kore 
-// // total price print koro
 
 
 const prices = [120, 450, 80, 320, 95];
@@ -9,7 +7,8 @@ let totalPrice = 0;
 for(let i = 0; i < prices.length; i++){
     totalPrice = totalPrice + prices[i];
 }
-console.log("Total Price : ", totalPrice);
+// console.log("Total Price : ", totalPrice);
+
 
 
 const scores = [45, 92, 67, 88, 23, 99, 54];
@@ -20,4 +19,33 @@ for(let i = 1; i < scores.length; i++){
     if(scores[i] > highestScore){
         highestScore = scores[i];
     }}
-console.log("Highest Score : ", highestScore);
+// console.log("Highest Score : ", highestScore);
+
+
+const numbers = [ 20, 50, 300, 54, 700, 20];
+
+let totalNumber = 0;
+
+for(let i = 0 ; i <  numbers.length; i++){
+    totalNumber = totalNumber + numbers[i];
+}
+console.log("Total Number :", totalNumber);
+
+
+const  number = [99, 40, 50, 90, 57, 89, 45];
+let num = number[0];
+for(let i = 0; i < number.length; i++){
+    if(number[i] < num){
+        num = number[i]
+    }
+}
+console.log("Sob Theke Choto Number", num);
+
+
+let y = [12, 50, 40, 60, 80, 90];
+
+let m = 0;
+for(let i =0; i < y.length; i++){
+    m = m + y[i];    
+}
+console.log("Total Sum", m);
