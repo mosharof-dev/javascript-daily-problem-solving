cd46ad1df22bf34041a9ec22fd657f95ddddaab8
programmermosharof <mosharof.dev@gmail.com>
2026-03-11 21:43:23 +0600

    day 8 problem Solveing done

--- day-8-problem.js
+++ day-8-problem.js
@@ -1,3 +1,5 @@
+//  ============== Day 8 Problem 1 ====================
+
 const products = [
   { name: "Laptop", price: 45000 },
   { name: "Keyboard", price: 2500 },
@@ -15,3 +17,21 @@ for(let product of products){
   let newPrice = calculateDiscount(product.price, 15)
   console.log( product.name + ":"+ newPrice);
 }
+
+//  ============== Day 8 Problem 2 ====================
+
+const  getGreeting = (hour) => {
+  if(hour >= 5 && hour <= 11){
+    return "Good Morning!"
+  }else if (hour >= 12 && hour <= 17){
+   return 'Good Afternoon!';
+  }else if (hour >= 18 && hour <= 21){
+    return 'Good Evening!';
+  }else{
+    return 'Good Night! '
+  }
+}
+console.log(getGreeting(8));  
+console.log(getGreeting(14)); 
+console.log(getGreeting(19)); 
+console.log(getGreeting(23));
