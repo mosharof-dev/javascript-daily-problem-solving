5f96395a039717de96bbe0520aa33a087ef5dfa7
programmermosharof <mosharof.dev@gmail.com>
2026-03-14 11:40:05 +0600

    day 10 Problem Solved

--- day-10-problem.js
+++ day-10-problem.js
@@ -11,3 +11,30 @@ const totalCost = orders.reduce((accumulator, current) => {
     return accumulator + current.price;
 }, 0) ;
 console.log("TotalCost:", totalCost );
+
+// ========== Day - 10  Problem = 2 ===========
+
+const students = [
+  { name: "Mosharof", js: 80, css: 75, html: 90 },
+  { name: "Rahim", js: 60, css: 55, html: 70 },
+  { name: "Karim", js: 90, css: 85, html: 95 }
+];
+
+const getReport = (students) => {
+    students.map((student) => {
+    
+    const total = student.html + student.css + student.js;
+    const avg = (total / 3).toFixed(2)
+
+    let  average = '';
+    if(avg >= 70){
+        average = "Pass"
+    }else{
+        average = "Fail"
+    }
+
+    console.log(`${student.name} — Average: ${avg} — ${average}`);
+})
+}
+     
+getReport(students)
