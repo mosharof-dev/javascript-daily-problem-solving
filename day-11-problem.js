8ef2162883e3cf0aa911d9b3ad49e3f32d67920e
programmermosharof <mosharof.dev@gmail.com>
2026-03-24 21:46:23 +0600

    Error: Bug fixed

--- day-11-problem.js
+++ day-11-problem.js
@@ -9,12 +9,13 @@ const students = [
 ];
 
 const totalMarks = students.reduce((accumulator, current)=>{
-   const totalMark = accumulator + current.marks;
-   const avg = totalMark / students.length;
-   console.log("Average Marks:",avg);
-   return avg, totalMark;
+    return accumulator + current.marks;  
 }, 0)
-console.log("Total Marks",totalMarks);
+
+const averageMarks = totalMarks / students.length;
+
+console.log("Average Marks:", averageMarks);
+console.log("Total Marks:",totalMarks);
 
 //  ===== Problem 2 — Function + filter() + map() =====
 const employees = [
@@ -24,7 +25,18 @@ const employees = [
   { name: "Sumon", salary: 12000, department: "HR" },
   { name: "Hasan", salary: 18000, department: "IT" }
 ];
-
+const getHRReport = () => {
+  return employees
+  .filter(employee => employee.department === "HR")
+  .map(employee => {
+    let updateSalary = employee.salary - (employee.salary * 10/ 100)
+    return{
+      ...employee,
+      salary: updateSalary
+    }
+  } )
+}
+console.log(getHRReport());
 
 const getITReport = () => {
  return employees
